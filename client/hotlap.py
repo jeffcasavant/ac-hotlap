d0f82956b4987d18611b4a82e0400b99684b14ef
Jeff Casavant <jeff.casavant@gmail.com>
2019-01-10 00:29:05 -0500

    Add unicodedata lib for encode on urllib; begin framework for displaying top laps

--- client/hotlap.py
+++ client/hotlap.py
@@ -16,7 +16,7 @@ from urllib import request
 from sim_info import info
 
 NAME = 'HotLap'
-SIZE = (200, 200)
+SIZE = (400, 200)
 
 DRIVER_ID_SELF = 0
 
@@ -28,7 +28,6 @@ CURRENT_LAP_VALID = True
 
 DRIVER      = ''
 TRACK       = ''
-TRACK_CONF  = ''
 CAR         = ''
 BEST_LOGGED_LAP = sys.maxsize
 LAP_HISTORY = []
@@ -37,20 +36,51 @@ SERVER = ''
 
 ACTIVE = True
 
+if not SERVER:
+    ac.console('Server is unset')
+
 def onActivate():
     global ACTIVE
     ACTIVE = True
+    ac.console('Activated')
 
 def onDeactivate():
     global ACTIVE
     ACTIVE = False
+    ac.console('Deactivated')
+
+def reportLap(lap_data):
+    data = json.dumps(lap_data)
+    req = request.Request(SERVER, data=data.encode('utf8'))
+    resp = request.urlopen(req)
+
+def getLapData():
+    url = SERVER + '?track=' + TRACK + '&car=' + CAR
+    req = request.Request(url)
+    resp = request.urlopen(req).read().decode('utf8')
+    return json.loads(resp)
+
+def msToHumanReadable(millis):
+    ms  = int(millis % 1000) / 100
+    sec = int(millis / 1000) % 60
+    min = int(millis / (1000 * 60)) % 60
+
+    return '{:02d}:{:02d}.{:03d}'.format(min, sec, ms)
+
+def refreshLapDisplay(lap_list):
+    laps = getLapData()
+
+    for lap in laps:
+        driver = lap['driver']
+        lap_time = msToHumanReadable(lap['lap'])
+        lap_label = '{} - {}'.format(driver, lap_time)
+        ac.addItem(lap_list, lap_label)
 
 def acMain(ac_version):
     global DRIVER, TRACK, CAR
 
     DRIVER     = ac.getDriverName(DRIVER_ID_SELF)
-    TRACK      = ac.getTrackName(DRIVER_ID_SELF)
-    TRACK_CONF = ac.getTrackConfiguration(DRIVER_ID_SELF)
+    TRACK      = ac.getTrackName(DRIVER_ID_SELF) + '-' + ac.getTrackConfiguration(DRIVER_ID_SELF)
     CAR        = ac.getCarName(DRIVER_ID_SELF)
 
     ac.console('Initialize {}: driver {} on {} in {}'.format(NAME, DRIVER, TRACK, CAR))
@@ -63,6 +93,15 @@ def acMain(ac_version):
         ac.console('Failed to add listener activate')
     if ac.addOnAppDismissedListener(window, onDeactivate) == -1:
         ac.console('Failed to add listener deactivate')
+
+    reportLap({'driver': DRIVER,
+               'track': TRACK,
+               'track_conf': TRACK_CONF,
+               'car': CAR,
+               'lap': 300000})
+
+    lap_list = ac.addListBox(window, 'Laps')
+    refreshLapDisplay(lap_list)
 
     return NAME
 
@@ -83,18 +122,15 @@ def acUpdate(ms):
         valid = CURRENT_LAP_VALID and not ac.getCarState(DRIVER_ID_SELF, acsys.CS.LapInvalidated)
 
         if last_lap and (not LAP_HISTORY or not last_lap == LAP_HISTORY[-1]):
+            refreshLapDisplay()
             if last_lap < BEST_LOGGED_LAP and valid:
 
                 ac.console('New record: {}'.format(last_lap))
-
-                req = request.Request(SERVER,
-                                      data=json.dumps({'driver': DRIVER,
-                                                       'track': TRACK,
-                                                       'track_conf': TRACK_CONF,
-                                                       'car': CAR,
-                                                       'lap': last_lap}))
-                resp = request.urlopen(req)
-                ac.console(resp)
+                reportLap({'driver': DRIVER,
+                           'track': TRACK,
+                           'track_conf': TRACK_CONF,
+                           'car': CAR,
+                           'lap': last_lap})
 
                 BEST_LOGGED_LAP = last_lap
             else:
